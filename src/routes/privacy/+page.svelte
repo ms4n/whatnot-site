f1de6decb7bf4adb5d5e59f869eef3713aa54953
ms4n <arjunsanjay0@gmail.com>
2023-04-27 02:09:37 +0530

    added privacy policy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,66 @@
+<script>
+</script>
+
+<main class="flex flex-col h-screen items-center justify-center">
+	<p class="mt-10 text-7xl text-green-500">privacy policy</p>
+	<p class="m-0 text-xl text-gray-500">last updated: 27 April 2023</p>
+	<div class="m-20">
+		<h2>Privacy Policy for WhatNot App</h2>
+		<p>
+			At WhatNot App, we are committed to protecting your privacy. This Privacy Policy explains how
+			we collect, use, and share your personal information when you use our service.
+		</p>
+		<h3>Information we collect</h3>
+		<p>When you use our service, we may collect the following types of information:</p>
+		<ul>
+			<li>
+				Personal information: We may collect your name, email address, and phone number when you
+				sign up for our service.
+			</li>
+			<li>
+				WhatsApp data: We may collect data from your WhatsApp account, including text messages,
+				media files, and other information necessary to provide our service.
+			</li>
+		</ul>
+		<h3>How we use your information</h3>
+		<p>We use your information to provide, maintain, and improve our service, including:</p>
+		<ul>
+			<li>Linking your WhatsApp account to your Google Suite account</li>
+			<li>Saving text messages to Google Docs</li>
+			<li>Creating Google Calendar events</li>
+			<li>Uploading media files to Google Drive</li>
+		</ul>
+		<p>
+			We may also use your information to communicate with you about our service, to troubleshoot
+			issues, and to send you marketing messages about our service (if you have opted in to receive
+			them).
+		</p>
+		<h3>How we share your information</h3>
+		<p>
+			We may share your information with third-party service providers who help us operate our
+			service, such as cloud hosting providers and analytics providers. We may also share your
+			information with third parties as required by law or to protect our legal rights.
+		</p>
+		<h3>Your rights</h3>
+		<p>
+			You have the right to access, correct, and delete your personal information. You may also opt
+			out of receiving marketing messages from us at any time.
+		</p>
+		<h3>Security</h3>
+		<p>
+			We take reasonable measures to protect your information from unauthorized access, disclosure,
+			and misuse. However, no security measures are perfect and we cannot guarantee the security of
+			your information.
+		</p>
+		<h3>Changes to this Privacy Policy</h3>
+		<p>
+			We may update this Privacy Policy from time to time to reflect changes in our service or
+			applicable laws. We will notify you of any material changes by email or through our service.
+		</p>
+		<h3>Contact us</h3>
+		<p>
+			If you have any questions or concerns about this Privacy Policy or our service, please contact
+			us at app.whatnot@gmail.com
+		</p>
+	</div>
+</main>
