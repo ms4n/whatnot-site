b5f7435cd97e6a3a4657677b716c764c3656e0e3
Sanjay <arjunsanjay0@gmail.com>
2023-08-05 04:33:22 +0530

    installed ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <>
-  <div>
-    <p className="text-4xl text-green-500">WhatNot</p>
-  </div>
-  </>
-  )
-  
+    <div className='bg-gray-100 h-screen p-10'>
+      <p className='text-4xl text-green-500'>WhatNot</p>
+      <Button className='bg-green-500 m-4'>Get Started!</Button>
+    </div>
+  );
 }
 
 export default App;
