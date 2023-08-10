aac76ab74cd1a30fb1f0d0327d2be602caaee395
Sanjay <arjunsanjay0@gmail.com>
2023-08-10 14:09:03 +0530

    added grainy texture to home

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <div className='bg-gray-100 h-screen p-10'>
-      <p className='text-4xl text-green-500'>WhatNot</p>
-      <Button className='bg-green-500 m-4'>Get Started!</Button>
+    <div className='py-10 noise h-screen m-12'>
+      <p className='text-5xl font-bold text-white'>WhatNot</p>
+      <Button className='bg-white text-green-500 m-4'>Get Started!</Button>
     </div>
   );
 }
