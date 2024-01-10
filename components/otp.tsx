7a894924f0a17dd44dd2725b82325e87066c746b
sanjay <arjunsanjay0@gmail.com>
2024-01-10 11:47:13 +0530

    added otp input

--- components/otp.tsx
+++ components/otp.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ChangeEvent, useState } from "react";
+import OTPInput from "react-otp-input";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -23,6 +24,18 @@ const OtpVerify: React.FC<OtpVerifyProps> = ({ onEditPhoneNumber }) => {
   const countryCode = useAppSelector((state) => state.phoneNumber.countryCode);
   const phoneNumber = useAppSelector((state) => state.phoneNumber.phoneNumber);
 
+  const [otp, setOtp] = useState("");
+
+  const customInputStyle: React.CSSProperties = {
+    width: "40px",
+    height: "40px",
+    border: "2px solid #22c55e",
+    borderRadius: "8px",
+    margin: "6px",
+    fontSize: "16px",
+    color: "black",
+  };
+
   return (
     <main className="flex items-center justify-center text-left">
       <Card>
@@ -33,7 +46,7 @@ const OtpVerify: React.FC<OtpVerifyProps> = ({ onEditPhoneNumber }) => {
           <CardDescription className="">
             Enter the OTP sent to{" "}
             <span>
-              {countryCode} {phoneNumber}.{" "}
+              {countryCode} {phoneNumber}{" "}
             </span>
             <span
               className="underline cursor-pointer"
@@ -44,9 +57,23 @@ const OtpVerify: React.FC<OtpVerifyProps> = ({ onEditPhoneNumber }) => {
           </CardDescription>
         </CardHeader>
 
-        <CardFooter className="flex flex-col">
+        <CardContent className="flex justify-center">
+          <OTPInput
+            value={otp}
+            onChange={setOtp}
+            numInputs={4}
+            inputStyle={customInputStyle}
+            renderInput={(props) => <Input {...props} />}
+          />
+        </CardContent>
+
+        <CardFooter className="flex flex-col gap-4">
+          <CardDescription>
+            OTP Valid for 300 seconds.{" "}
+            <span className="underline cursor-pointer">Resend</span>
+          </CardDescription>
           <Button className="bg-green-500 text-white hover:scale-105 transition ring-2 ring-green-300">
-            Send OTP
+            Verify OTP
           </Button>
         </CardFooter>
       </Card>
